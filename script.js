09ce52d70926ebc0d84db35ecb16aba0b514b8ed
AhmedAdel-WebDev <ahmedginidi1112003111@gmail.com>
2024-08-05 19:28:27 +0300

    adding student 1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ student1 = {
   name: "Ahmed adel ahmed",
   email: "ahmedginidi1112003111@gmail.com",
   age: 20,
-  track: "React",
+  track: "React web",
 };
 student2 = {
   name: "Mohamed Shams El-Deen",
